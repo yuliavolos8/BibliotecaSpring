0e0189bfb311d57cfa0527a4706d47f4ea853f4a
yuliavolos8 <147557438+yuliavolos8@users.noreply.github.com>
2024-06-03 19:50:17 +0200

    proyecto biblioteca

---
+++ src/test/java/bibliotecaSpring/BibliotecaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package bibliotecaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
